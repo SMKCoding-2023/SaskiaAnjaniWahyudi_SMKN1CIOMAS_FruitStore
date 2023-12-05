b43ae9f49e992fd954d845344f2b2e71186fdca9
saskiaanjani <wahyudianjani54@gmail.com>
2023-12-05 17:26:16 +0700

    initial commit

---
+++ ecommerce/Pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hero w-full bg-gradient-to-r from-yellow-200 via-gren-300 to-blue-400 min-h-screen flex flex-col">
+    <div class="hero-content flex-col lg:flex-row mt-10">
+      <div class="w-[600px] ml-[60px]">
+        <h1 class="text-5xl font-bold text-red-800">Selamat Datang di Fruit Store</h1>
+        <p class="mt-4 text-lg text-yellow-700">Pilihlah buah kesukaan anda.</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ ecommerce/composables/constants/category.ts
@@ -0,0 +1,10 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Buah",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
